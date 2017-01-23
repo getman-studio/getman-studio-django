b105e1bcf2561c909d8012beb8d58d565cc8bc98
david <ariedov@gmail.com>
2017-01-23 21:19:55 +0200

    adds category model to shop

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from models import Category
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"text_id": ("name",)}
+
+admin.site.register(Category, CategoryAdmin)
--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,18 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=15)
+    text_id = models.SlugField(max_length=15)
+    position = models.IntegerField(default=0)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = u'Категорія'
+        verbose_name_plural = u'Категорії'
+        ordering = ('position', )
+
+    def __unicode__(self):
+       return self.name
