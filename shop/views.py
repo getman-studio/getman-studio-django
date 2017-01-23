5ff7eb42a5ce8b85a38416c85021b304b2d6886c
david <ariedov@gmail.com>
2017-01-23 22:44:48 +0200

    shows category items

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from models import Category
+from models import Category, Item
 
 def shop(request):
     categories = Category.objects.all()
+    for category in categories:
+        category.items = Item.objects.filter(category__id=category.id)
     return render(request, 'shop/shop.html', {'categories': categories})
