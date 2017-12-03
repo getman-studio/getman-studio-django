0ba63eeb09829664f220f21f64724d93403b88e2
david <ariedov@gmail.com>
2017-12-03 23:58:59 +0200

    adds str method to shop items

--- shop/admin.py
+++ shop/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from .models import Category, Item
 
+
 # Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"text_id": ("name",)}
 
+
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'price')
+
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Item, ItemAdmin)
--- shop/models.py
+++ shop/models.py
@@ -14,6 +14,9 @@ class Category(models.Model):
         verbose_name_plural = u'Категорії'
         ordering = ('position',)
 
+    def __str__(self):
+        return self.name
+
     def __unicode__(self):
         return self.name
 
@@ -28,6 +31,9 @@ class Item(models.Model):
     class Meta:
         verbose_name = u'Штука'
         verbose_name_plural = u'Штуки'
+
+    def __str__(self):
+        return self.name
 
     def __unicode__(self):
         return self.name
