ab37634b579b588a606eedcb0df2882af464080b
david <ariedov@gmail.com>
2019-04-30 18:25:17 +0300

    preparing for https

--- getman_studio/wsgi.py
+++ getman_studio/wsgi.py
@@ -13,6 +13,7 @@ from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "getman_studio.settings")
+os.environ['HTTPS'] = "on"
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
